99f6bd6db7fea75151a6437eba70c526203974c2
Juan Mendoza <64992531+jsmendozap@users.noreply.github.com>
2021-01-12 21:44:50 -0500

    Update configuracion.sh

--- configuracion.sh
+++ configuracion.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/bash
 
-sudo apt-get install python3 r-base rpl -y
+sudo apt-get install python3 r-base rpl libcurl4-openssl-dev libssl-dev iwatch cron openssh-server -y
 chmod +x paquetes.R watch.R
 sudo ./paquetes.R
